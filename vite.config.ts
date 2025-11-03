d676a09198c3cf614a28f13650f26d52fc6a9ae3
ManuelRuiz27 <manuel_ruiz27@live.com>
2025-11-02 20:01:18 -0600

    m

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Drag_n_Drop/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173
